9d5f9e6a9e6a36fc12302f33369a5e223338745a
Nitesh Vijay <niteshvijay1995@gmail.com>
2018-05-02 18:22:25 +0530

    Add NVPictureInPicture

---
+++ NVPictureInPicture.h
@@ -0,0 +1,12 @@
+//
+//  NVPictureInPicture.h
+//  NVPictureInPicture
+//
+//  Created by Nitesh Vijay on 02/05/18.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NVPictureInPicture : NSObject
+
+@end
---
+++ NVPictureInPicture.m
@@ -0,0 +1,12 @@
+//
+//  NVPictureInPicture.m
+//  NVPictureInPicture
+//
+//  Created by Nitesh Vijay on 02/05/18.
+//
+
+#import "NVPictureInPicture.h"
+
+@implementation NVPictureInPicture
+
+@end
