977639e06bcf48e4e428f9ff85a40d6b2c2357bf
Nitesh Vijay <niteshvijay1995@gmail.com>
2018-05-02 18:21:38 +0530

    Add NVPIPWindow

---
+++ NVPIPWindow.h
@@ -0,0 +1,12 @@
+//
+//  NVPIPWindow.h
+//  NVPictureInPicture
+//
+//  Created by Nitesh Vijay on 02/05/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NVPIPWindow : UIWindow
+
+@end
---
+++ NVPIPWindow.m
@@ -0,0 +1,20 @@
+//
+//  NVPIPWindow.m
+//  NVPictureInPicture
+//
+//  Created by Nitesh Vijay on 02/05/18.
+//
+
+#import "NVPIPWindow.h"
+
+@implementation NVPIPWindow
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
