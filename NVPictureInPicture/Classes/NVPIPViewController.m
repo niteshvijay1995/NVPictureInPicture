c94382d0f7f79836d5ac54e7995df1948ea0db52
Nitesh Vijay <niteshvijay1995@gmail.com>
2018-05-03 12:29:23 +0530

    Handle pan gesture for display mode expanded

--- NVPictureInPicture/Classes/NVPIPViewController.m
+++ NVPictureInPicture/Classes/NVPIPViewController.m
@@ -7,7 +7,9 @@
 
 #import "NVPIPViewController.h"
 
-static const CGSize defaultSizeInCompactMode = {100, 150};
+static const CGSize DefaultSizeInCompactMode = {100, 150};
+static const CGFloat PanSensitivity = 2.0f;
+static const CGFloat ThresholdPercentForDisplayModeCompact = 0.5;
 
 typedef NS_ENUM(NSInteger, NVPIPDisplayMode) {
   NVPIPDisplayModeExpanded,
@@ -18,8 +20,9 @@ typedef NS_ENUM(NSInteger, NVPIPDisplayMode) {
 
 @property (nonatomic) NVPIPDisplayMode displayMode;
 @property (nonatomic) UIPanGestureRecognizer *panGesture;
-@property (nonatomic) CGSize compactModeSize;
-@property (nonatomic) CGSize expandedModeSize;
+@property (nonatomic) CGRect compactModeFrame;
+@property (nonatomic) CGRect expandedModeFrame;
+@property (nonatomic) UIEdgeInsets edgeInsets;
 
 @end
 
@@ -27,16 +30,32 @@ typedef NS_ENUM(NSInteger, NVPIPDisplayMode) {
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  self.compactModeSize = [self sizeInDisplayModeCompact];
-  self.expandedModeSize = [UIScreen mainScreen].bounds.size;
+  self.edgeInsets = [self edgeInsetsForDisplayModeCompact];
+  self.compactModeFrame = [self frameForDisplayMode:NVPIPDisplayModeCompact];
+  self.expandedModeFrame = [self frameForDisplayMode:NVPIPDisplayModeExpanded];
   self.displayMode = NVPIPDisplayModeExpanded;
-  self.view.frame = CGRectMake(0, 0, self.expandedModeSize.width, self.expandedModeSize.height);
+  self.view.frame = self.expandedModeFrame;
   self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
   [self.view addGestureRecognizer:self.panGesture];
 }
 
-- (CGSize)sizeInDisplayModeCompact {
-  return defaultSizeInCompactMode;
+- (CGRect)frameForDisplayMode:(NVPIPDisplayMode)displayMode {
+  CGSize screenSize = [UIScreen mainScreen].bounds.size;
+  if (displayMode == NVPIPDisplayModeCompact) {
+    return CGRectMake(screenSize.width - self.edgeInsets.right - DefaultSizeInCompactMode.width,
+                      screenSize.height - self.edgeInsets.bottom - DefaultSizeInCompactMode.height,
+                      DefaultSizeInCompactMode.width,
+                      DefaultSizeInCompactMode.height);
+  } else {
+    return CGRectMake(0,
+                      0,
+                      screenSize.width,
+                      screenSize.height);
+  }
+}
+
+- (UIEdgeInsets)edgeInsetsForDisplayModeCompact {
+  return UIEdgeInsetsMake(30, 10, 30, 10);
 }
 
 - (void)handlePan:(UIGestureRecognizer *)gestureRecognizer {
@@ -48,7 +67,20 @@ typedef NS_ENUM(NSInteger, NVPIPDisplayMode) {
 }
 
 - (void)handlePanForDisplayModeExpanded:(UIPanGestureRecognizer *)gestureRecognizer {
-  
+  if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
+    [self.panGesture setTranslation:CGPointZero inView:self.view];
+  } else {
+    CGPoint translation = [gestureRecognizer translationInView:self.view];
+    CGFloat percentage = PanSensitivity * fabs(translation.y / (CGRectGetHeight(self.expandedModeFrame) - CGRectGetHeight(self.compactModeFrame)));
+    if (gestureRecognizer.state == UIGestureRecognizerStateChanged
+        && percentage <= 1) {
+      [self updateViewWithTranslationPercentage:percentage];
+    } else if (gestureRecognizer.state == UIGestureRecognizerStateEnded
+               || gestureRecognizer.state == UIGestureRecognizerStateCancelled
+               || gestureRecognizer.state == UIGestureRecognizerStateFailed) {
+      [self setDisplayModeWithTranslationPercentage:percentage];
+    }
+  }
 }
 
 - (void)handlePanForDisplayModeCompact:(UIPanGestureRecognizer *)gestureRecognizer {
@@ -64,6 +96,32 @@ typedef NS_ENUM(NSInteger, NVPIPDisplayMode) {
              || gestureRecognizer.state == UIGestureRecognizerStateFailed) {
     return;
   }
+}
+
+- (void)updateViewWithTranslationPercentage:(CGFloat)percentage {
+  CGSize sizeDifference = CGSizeMake(CGRectGetWidth(self.expandedModeFrame) - CGRectGetWidth(self.compactModeFrame),
+                                     CGRectGetHeight(self.expandedModeFrame) - CGRectGetHeight(self.compactModeFrame));
+  CGPoint originDifference = CGPointMake(CGRectGetMinX(self.expandedModeFrame) - CGRectGetMinX(self.compactModeFrame),
+                                         CGRectGetMinY(self.expandedModeFrame) - CGRectGetMinY(self.compactModeFrame));
+  self.view.frame = CGRectMake(CGRectGetMinX(self.expandedModeFrame) - originDifference.x * percentage,
+                               CGRectGetMinY(self.expandedModeFrame) - originDifference.y * percentage,
+                               CGRectGetWidth(self.expandedModeFrame) - sizeDifference.width * percentage,
+                               CGRectGetHeight(self.expandedModeFrame) - sizeDifference.height * percentage);
+}
+
+- (void)setDisplayModeWithTranslationPercentage:(CGFloat)percentage {
+  if (percentage > ThresholdPercentForDisplayModeCompact) {
+    self.displayMode = NVPIPDisplayModeCompact;
+  } else {
+    self.displayMode = NVPIPDisplayModeExpanded;
+  }
+  [self animateViewToDisplayMode:self.displayMode];
+}
+
+- (void)animateViewToDisplayMode:(NVPIPDisplayMode)displayMode {
+  [UIView animateWithDuration:0.2 animations:^{
+    self.view.frame = [self frameForDisplayMode:displayMode];
+  }];
 }
 
 - (BOOL)shouldReceivePoint:(CGPoint)point {
