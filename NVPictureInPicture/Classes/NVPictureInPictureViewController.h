e33fafad28f98927babb46d62367b97814ef1d90
Nitesh Vijay <niteshvijay1995@gmail.com>
2018-05-10 11:27:41 +0530

    Add NVPictureInPictureViewController

---
+++ NVPictureInPicture/Classes/NVPictureInPictureViewController.h
@@ -0,0 +1,12 @@
+//
+//  NVPictureInPictureViewController.h
+//  NVPictureInPicture
+//
+//  Created by Nitesh Vijay on 10/05/18.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NVPictureInPictureViewController : UIViewController
+
+@end
---
+++ NVPictureInPicture/Classes/NVPictureInPictureViewController.m
@@ -0,0 +1,17 @@
+//
+//  NVPictureInPictureViewController.m
+//  NVPictureInPicture
+//
+//  Created by Nitesh Vijay on 10/05/18.
+//
+
+#import "NVPictureInPictureViewController.h"
+
+@interface NVPictureInPictureViewController ()
+
+@end
+
+@implementation NVPictureInPictureViewController
+
+
+@end
